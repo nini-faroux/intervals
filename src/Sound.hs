1e3bf7f12ae97226190e32423266477353b390d5
nini-faroux <nfarrel1@tcd.ie>
2021-05-24 16:22:03 +0100

    add sdl2 sound module

---
+++ src/Sound.hs
@@ -0,0 +1,26 @@
+module Sound where
+
+import qualified SDL.Mixer as Mix
+import qualified SDL
+
+import Control.Monad      (when)
+import Data.Default.Class (def)
+
+intervalSound :: FilePath -> IO ()
+intervalSound fp = do
+  SDL.initialize [SDL.InitAudio]
+  Mix.initialize [Mix.InitFLAC]
+  Mix.openAudio def 256
+  sound <- Mix.load fp
+  Mix.play sound
+  loopSound $ Mix.playing Mix.AllChannels
+
+  Mix.free sound
+  Mix.closeAudio
+  Mix.quit
+  SDL.quit
+
+loopSound :: Monad m => m Bool -> m ()
+loopSound condM = do
+  cond <- condM
+  when cond $ loopSound condM
