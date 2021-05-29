a18be0ef8afa894c58572ea4facf627594ae0653
nini-faroux <nfarrel1@tcd.ie>
2021-05-29 17:10:48 +0100

    clean up sound

--- src/Sound.hs
+++ src/Sound.hs
@@ -7,21 +7,17 @@ import qualified SDL
 import Control.Monad      (when)
 import Data.Default.Class (def)
 
-intervalSound :: FilePath -> IO ()
-intervalSound fp = do
+initializeSound :: IO ()
+initializeSound = do
   SDL.initialize [SDL.InitAudio]
   Mix.initialize [Mix.InitFLAC]
   Mix.openAudio def 256
-  sound <- Mix.load fp
-  Mix.play sound
-  loopSound $ Mix.playing Mix.AllChannels
 
-  Mix.free sound
+playSound :: FilePath -> IO ()
+playSound fp = Mix.load fp >>= \s -> Mix.play s
+
+freeSound :: IO ()
+freeSound = do
   Mix.closeAudio
   Mix.quit
   SDL.quit
-
-loopSound :: Monad m => m Bool -> m ()
-loopSound condM = do
-  cond <- condM
-  when cond $ loopSound condM
