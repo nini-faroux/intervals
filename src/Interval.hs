e13cb0a988968d19c765a6404d7b481920b465e8
nini-faroux <nfarrel1@tcd.ie>
2021-05-31 22:50:46 +0100

    use RecordWildCards

--- src/Interval.hs
+++ src/Interval.hs
@@ -21,13 +21,13 @@ runApp app = initializeSound >> runRIO app runIntervals
 
 runIntervals :: RIO App ()
 runIntervals = do
-  App numInts lenOn lenOff ss es songs <- ask
-  countVar <- liftIO $ newTMVarIO (numInts * 2, On)
+  App {..} <- ask
+  countVar <- liftIO $ newTMVarIO (numIntervals * 2, On)
   concurrently' (intervals countVar) (liftIO $ playSongs songs)
 
 intervals :: TMVar (Int, Switch) -> RIO App ()
 intervals countVar = do
-  App _ lenOn lenOff ss es songs <- ask
+  App {..} <- ask
   (numIntervals, _) <- atomically $ readTMVar countVar
   when (numIntervals == 0) (liftIO exitProgram)
   runThreads countVar
@@ -35,11 +35,11 @@ intervals countVar = do
 
 runThreads :: TMVar (Int, Switch) -> RIO App ()
 runThreads countVar = do
-  App _ lenOn lenOff ss es songs <- ask
+  App {..} <- ask
   (numIntervals, switch) <- atomically $ takeTMVar countVar
   case switch of
-    On -> conc On ss lenOn
-    Off -> conc Off es lenOff 
+    On -> conc On startSoundFile lengthOn
+    Off -> conc Off endSoundFile lengthPause 
   atomically $ putTMVar countVar (numIntervals - 1, toggle switch)
   where
     conc s fp time = concurrently' (liftIO $ go s fp) (liftIO $ count fp time (show s))
